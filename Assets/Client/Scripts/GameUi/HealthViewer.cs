2ba144b4faee40355ff3d412f6808fdec3f07b72
Myrzabek Alizhanov <mlgexgood@gmail.com>
2022-05-18 04:53:49 +0600

    Night Sky, VFX, UI Base

---
+++ Assets/Client/Scripts/GameUi/HealthViewer.cs
@@ -0,0 +1,10 @@
+using Client.Scripts.Wizard;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthViewer : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private WizardBehaviour _wizardBehaviour;
+
+}
